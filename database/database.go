63950699358ecfea6172b5bfa9831b71cc7dda90
stinkyfingers <john_shenk@hotmail.com>
2016-01-06 16:12:56 -0600

    first commit

---
+++ database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	EmptyDb = flag.String("clean", "", "bind empty database with structure defined")
+	DB      *sql.DB
+)
+
+type Scanner interface {
+	Scan(...interface{}) error
+}
+
+func ConnectionString() string {
+	if addr := os.Getenv("DATABASE_HOST"); addr != "" {
+		proto := os.Getenv("DATABASE_PROTOCOL")
+		user := os.Getenv("DATABASE_USERNAME")
+		pass := os.Getenv("DATABASE_PASSWORD")
+		db := os.Getenv("CURT_DEV_NAME")
+
+		return fmt.Sprintf("%s:%s@%s(%s)/%s?parseTime=true&loc=%s", user, pass, proto, addr, db, "America%2FChicago")
+	}
+	return "root:@tcp(127.0.0.1:3306)/CurtData?parseTime=true&loc=America%2FChicago"
+}
+
+func Init() error {
+	var err error
+	if DB == nil {
+		DB, err = sql.Open("mysql", ConnectionString())
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
